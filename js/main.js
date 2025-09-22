9b677f6c46203664c6337b9d72ab0797e6602dc3
Karen Heredia <karen.julieth.heredia@gmail.com>
2025-09-22 13:24:59 -0500

    Agregué la estructura base del proyecto desde el repositorio del instructor

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const buttonDelete = document.getElementById('buttonDelete');
+const buttonBuy = document.getElementById('buttonBuy');
+const itemVehicle = document.getElementById('itemVehicle');
+const buttonCreateVehicle = document.getElementById('buttonCreateVehicle');
+const bodyCard = document.getElementById('bodyCard')
+const cardInfo = document.getElementById('cardInfo')
+const constButtons = document.getElementById('constButtons')
+
+ 
