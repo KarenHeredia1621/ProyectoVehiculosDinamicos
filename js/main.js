c3df0363da89ef28abaad0cce3cfd496fbe32213
Karen Heredia <karen.julieth.heredia@gmail.com>
2025-09-30 19:40:00 -0500

    Agregué la persistencia de los vehículos en el carrito usando localStorage e hice que los vehículos se mantengan al recargar la página. Implementé la eliminación de vehículos por id, actualizando el DOM y el almacenamiento. Corregí el error en setItem pasando correctamente la clave y el valor y por último actualicé dinámicamente el contador de productos y el total del carrito.

--- js/main.js
+++ js/main.js
@@ -101,14 +101,12 @@ form.addEventListener('submit', (m) => {
         if (foto === '') {
             foto = 'https://fotos.quecochemecompro.com/jeep-compass/jeep-compass-trasera.jpg?size=1200x800'
         }
-        const newVehicle = createNewVehicle(foto, nombre, marca, modelo, kilometraje, precio)
-        containerCards.appendChild(newVehicle)
-        form.reset()
-        eventsToCard(newVehicle)
-    }
 
-    //Anteriormente era un solo valor y ahora estamos alimentando un objeto con sus diferentes claves o valores
-    const newVehicle = {
+
+
+        //Anteriormente era un solo valor y ahora estamos alimentando un objeto con sus diferentes claves o valores
+    const vehiculos = {
+        id: Date.now(),
         foto : foto,
         nombre : nombre,
         marca : marca,
@@ -121,32 +119,112 @@ form.addEventListener('submit', (m) => {
     const vehiculosGuardados = JSON.parse(localStorage.getItem("vehiculos")) || [];
 
     //Agregamos al arreglo[] vehiculosGuardados
-    vehiculosGuardados.push(newVehicle);
+    vehiculosGuardados.push(vehiculos);
 
     localStorage.setItem("vehiculos", JSON.stringify(vehiculosGuardados));
-    const vehiculoCreado = localStorage.getItem("vehiculo");
+    
 
 
     form.reset();
+        const newVehicle = createNewVehicle(foto, nombre, marca, modelo, kilometraje, precio)
+        containerCards.appendChild(newVehicle)
+        form.reset()
+        eventsToCard(newVehicle)
+    }
+
+    
 
 });
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    const vehiculoCreado = localStorage.getItem("vehiculo");
-    if (vehiculoCreado) {
-        const objetoVehiculo = JSON.parse(vehiculoCreado)
-        inputMensaje.innerHTML = "El vehículo es: " + objetoVehiculo.foto + "<br>" + "nombre: " + objetoVehiculo.nombre + "<br>" + "marca: " + objetoVehiculo.marca + "<br>" + "modelo: " + objetoVehiculo.modelo + "<br>" + "Kilometraje: " + objetoVehiculo.kilometraje + "<br>" + "Precio: " + objetoVehiculo.precio;
+    const vehiculosGuardados = JSON.parse(localStorage.getItem("vehiculos")) || []
 
-    }
+    vehiculosGuardados.forEach((vehiculo) => {
+        // creo el elemento padre de la tarjeta
+    const containerTarget = document.createElement('div');
+    containerTarget.classList.add('col-md-6', 'item-vehiculo');
+    containerTarget.dataset.id = vehiculo.id
+    // creo el elemento hijo que contiene todo el cuerpo de la informacion con la foto 
+    const bodyCard = document.createElement('div');
+    bodyCard.classList.add('card', 'h-100');
+    // creamos la imagen
+    const img = document.createElement('img');
+    img.classList.add("fotos");
+    img.src = vehiculo.foto;
+    document.body.appendChild(img);
+    // creamos el contenedor de toda la informacion del vehiculo
+    const carInfo = document.createElement('div');
+    carInfo.classList.add('card-body');
+    // creamos el h4 que contiene el nombre del vehiculo
+    const nameVehicle = document.createElement('h3');
+    nameVehicle.classList.add('card-title');
+    nameVehicle.textContent = 'Nombre: ' + vehiculo.nombre
+    // creamos el campo para la marca del vehiculo
+    const brandVehicle = document.createElement('h4');
+    brandVehicle.classList.add('card-subtitle', 'mb-2');
+    brandVehicle.textContent = 'Marca: ' + vehiculo.marca
+    // creamos el campo para el modelo del vehiculo
+    const modelVehicle = document.createElement('h4');
+    modelVehicle.classList.add('card-text');
+    modelVehicle.textContent = 'Modelo: ' + vehiculo.modelo
+    // creamos el campo para el kilometraje del vehiculo
+    const kmVehicle = document.createElement('h4');
+    kmVehicle.classList.add('card-km');
+    kmVehicle.textContent = 'Recorrido: ' + vehiculo.kilometraje + ' km'
+    // creamos el campo para el precio del vehiculo
+    const priceVehicle = document.createElement('h4');
+    priceVehicle.classList.add('text-success');
+    priceVehicle.textContent = 'Precio: $' + vehiculo.precio
+    // creamos el contenedor de los dos botones, comprar y eliminar
+    const containerButtons = document.createElement('div');
+    containerButtons.classList.add('d-flex', 'justify-content-between', 'mt-3')
+    // creamos el boton de comprar
+    const buttonBuy = document.createElement('button');
+    buttonBuy.classList.add('btn', 'btn-success')
+    buttonBuy.id = 'button_buy'
+    buttonBuy.textContent = 'Comprar';
+    // creamos el boton de eliminar
+    const buttonDeleteCard = document.createElement('button');
+    buttonDeleteCard.classList.add('btn', 'btn-danger')
+    buttonDeleteCard.id = 'button_delete'
+    buttonDeleteCard.textContent = 'Eliminar';
+
+    containerCards.appendChild(containerTarget)
+
+    containerTarget.appendChild(bodyCard)
+    bodyCard.appendChild(img)
+    bodyCard.appendChild(carInfo)
+    carInfo.appendChild(nameVehicle);
+    carInfo.appendChild(brandVehicle)
+    carInfo.appendChild(modelVehicle)
+    carInfo.appendChild(kmVehicle)
+    carInfo.appendChild(priceVehicle)
+    carInfo.appendChild(containerButtons)
+    containerButtons.appendChild(buttonBuy)
+    containerButtons.appendChild(buttonDeleteCard)
+
+    eventsToCard(containerTarget, vehiculo.id);
+
+    return containerTarget;
+    })
+
+    
+    
 });
 
 
-function eventsToCard(containerTarget) {
+function eventsToCard(containerTarget, idVehiculo) {
     const buttonBuy = containerTarget.querySelector('#button_buy')
     const buttonDelete = containerTarget.querySelector('#button_delete');
 
     buttonDelete.addEventListener('click', () => {
         containerTarget.remove()
+
+        let vehiculosGuardados = JSON.parse(localStorage.getItem("vehiculos")) || [];
+
+        vehiculosGuardados = vehiculosGuardados.filter(v => v.id !== idVehiculo);
+
+        localStorage.setItem("vehiculos" , JSON.stringify(vehiculosGuardados));
     })
 
     buttonBuy.addEventListener('click', () => {
@@ -162,14 +240,112 @@ function eventsToCard(containerTarget) {
         contProducts.appendChild(cartItem)
         countProducts.textContent = parseInt(countProducts.textContent) + 1;
 
+        const newVehicleCard = {
+        id: Date.now(),
+        foto : foto,
+        nombre : nombre,
+        marca : marca,
+        modelo : modelo,
+        kilometraje : kilometraje,
+        precio : precio
+    }
+
+    const vehiculosGuardadosCard = JSON.parse(localStorage.getItem("vehiculosCard")) || [];
+    vehiculosGuardadosCard.push(newVehicleCard);
+
+        localStorage.setItem("vehiculosCard", JSON.stringify(vehiculosGuardadosCard));
+
+
 
         total += parseInt(precio)
         totalPrice.textContent = total
-        // for (let i = precio; i <= countProducts; i++) {
-        //     i + precio
-        // }
     })
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const vehiculosGuardadosEnCarrito = JSON.parse(localStorage.getItem("vehiculosCard")) || []
+
+    vehiculosGuardadosEnCarrito.forEach((vehiculo) => {
+        // creo la tarjeta que va a tener los datos de cada vehiculo a comprar
+    const containerTarget = document.createElement('div');
+    containerTarget.classList.add('targetProduct');
+    // creo la etiqueta img
+    const img = document.createElement('img');
+    img.src = vehiculo.foto;
+    // creo el espacio para las imagenes
+    const containerImg = document.createElement('div');
+    containerImg.classList.add('col-md-5', 'container-img')
+    containerImg.appendChild(img)
+
+    // creo el espacio para la informacion del vehiculo
+    const containerInfoVehicle = document.createElement('div')
+    containerInfoVehicle.classList.add('col-md-7', 'carInfoProduct');
+
+    // creo el espacio para mostrar el nombre del vehiculo
+    const nameVehicleProduct = document.createElement('h4')
+    nameVehicleProduct.classList.add('card-title');
+    nameVehicleProduct.textContent = 'Vehiculo: ' + vehiculo.nombre
+    // creo el espacio para mostrar la marca del vehiculo
+    const brandVehicleProduct = document.createElement('h4');
+    brandVehicleProduct.classList.add('card-subtitle', 'mb-2')
+    brandVehicleProduct.textContent = 'Marca: ' + vehiculo.marca
+
+    const modelVehicleProduct = document.createElement('h4');
+    modelVehicleProduct.classList.add('card-text');
+    modelVehicleProduct.textContent = 'Modelo: ' + vehiculo.modelo
+    // creo el espacio para mostrar el kilometraje del vehiculo
+    const mileageCarProduct = document.createElement('h4');
+    mileageCarProduct.classList.add('card-km');
+    mileageCarProduct.textContent = 'Recorrido: ' + vehiculo.kilometraje + ' km';
+    // creo el espacio para mostrar el precio del vehiculo
+    const priceVehicleProduct = document.createElement('h4');
+    priceVehicleProduct.classList.add('text-success');
+    priceVehicleProduct.textContent = 'Precio: $' + vehiculo.precio
+    // creo el boton para eliminar la tarjeta
+    const buttonDeleteCard = document.createElement('button');
+    buttonDeleteCard.classList.add('btn', 'btn-danger')
+    buttonDeleteCard.id = 'button_delete'
+    buttonDeleteCard.textContent = 'Eliminar';
+    buttonDeleteCard.addEventListener('click', () => {
+        containerTarget.remove()
+        let vehiculosGuardados = JSON.parse(localStorage.getItem("vehiculosCard")) || [];
+
+        vehiculosGuardados = vehiculosGuardados.filter(v => v.id !== vehiculo.id)
+
+        localStorage.setItem("vehiculosCard" ,JSON.stringify(vehiculosGuardados));
+        countProducts.textContent = parseInt(countProducts.textContent) - 1
+        total -= parseInt(vehiculo.precio)
+        totalPrice.textContent = total
+    })
+
+    contProducts.appendChild(containerTarget);
+
+    containerInfoVehicle.appendChild(nameVehicleProduct)
+    containerInfoVehicle.appendChild(brandVehicleProduct)
+    containerInfoVehicle.appendChild(modelVehicleProduct)
+    containerInfoVehicle.appendChild(mileageCarProduct)
+    containerInfoVehicle.appendChild(priceVehicleProduct)
+    containerInfoVehicle.appendChild(buttonDeleteCard)
+    containerTarget.appendChild(containerImg)
+    containerTarget.appendChild(containerInfoVehicle)
+
+    // actualizar contador y total
+        countProducts.textContent = parseInt(countProducts.textContent) + 1;
+        total += parseInt(vehiculo.precio);
+        totalPrice.textContent = total;
+
+    
+
+    return containerTarget;
+    })
+    
+
+    
+})
+
+
 function addProduct(foto, nombre, marca, modelo, kilometraje, precio) {
     // traigo la informacion que necesito de otra funcion con el valor devuelto de la misma
     const newVehicleProduct = createNewVehicle(foto, nombre, marca, modelo,modelo, kilometraje, precio)
@@ -229,20 +405,7 @@ function addProduct(foto, nombre, marca, modelo, kilometraje, precio) {
     containerTarget.appendChild(containerImg)
     containerTarget.appendChild(containerInfoVehicle)
 
-    const newVehicleCard = {
-        foto : foto,
-        nombre : nombre,
-        marca : marca,
-        modelo : modelo,
-        kilometraje : kilometraje,
-        precio : precio
-    }
-
-    const vehiculosGuardadosCard = JSON.parse(localStorage.getItem("vehiculosCard")) || [];
-    vehiculosGuardadosCard.push(newVehicleCard);
-
-        localStorage.setItem("vehiculosCard", JSON.stringify(vehiculosGuardadosCard));
-
+    
 
     return containerTarget;
 }
@@ -250,4 +413,5 @@ function addProduct(foto, nombre, marca, modelo, kilometraje, precio) {
 buttonCarProduct.addEventListener('click', () => {
     side_bar.classList.toggle('active');
     containerBtnCar.classList.toggle('classContainerBtnCar');
-})
+});
+
--- js/tablaVehiculos.js
+++ js/tablaVehiculos.js
@@ -1,38 +1,41 @@
-const bodyTable = document.getElementById("tablaCarrito");
+const tablaBody = document.querySelector('#tablaVehiculos tbody')
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    const vehiculosGuardados = JSON.parse(localStorage.getItem("vehiculos"))
-    vehiculosGuardados.forEach(vehiculo =>{
-        const fila = document.createElement("tr");
-        const tdFoto = document.createElement("td");
-        const img = document.createElement("img");
-img.src = vehiculo.foto;   // Aquí pones la URL que tienes guardada
-img.alt = vehiculo.nombre; // Texto alternativo opcional
-img.width = 100;           // Opcional: tamaño de la imagen
-tdFoto.appendChild(img);
+document.addEventListener('DOMContentLoaded', () => {
+    const vehiculosGuardadosEnCarrito = JSON.parse(localStorage.getItem("vehiculosCard")) || [];
 
-        const tdNombre = document.createElement("td");
+    vehiculosGuardadosEnCarrito.forEach((vehiculo) => {
+        const trBody = document.createElement('tr');
+
+        const tdFoto = document.createElement('td');
+        const img = document.createElement('img');
+        img.src = vehiculo.foto;
+        img.style.width = '100px';
+        tdFoto.appendChild(img);
+        
+
+        const tdNombre = document.createElement('td');
         tdNombre.textContent = vehiculo.nombre;
 
-        const tdMarca = document.createElement("td");
+        const tdMarca = document.createElement('td');
         tdMarca.textContent = vehiculo.marca;
 
-        const tdModelo = document.createElement("td");
+        const tdModelo = document.createElement('td');
         tdModelo.textContent = vehiculo.modelo;
 
-        const tdKilometraje = document.createElement("td");
+        const tdKilometraje = document.createElement('td');
         tdKilometraje.textContent = vehiculo.kilometraje;
 
-        const tdPrecio = document.createElement("td");
+        const tdPrecio = document.createElement('td');
         tdPrecio.textContent = vehiculo.precio;
 
-        fila.appendChild(tdFoto);
-        fila.appendChild(tdNombre);
-        fila.appendChild(tdMarca);
-        fila.appendChild(tdModelo);
-        fila.appendChild(tdKilometraje);
-        fila.appendChild(tdPrecio);
-        
-        bodyTable.appendChild(fila);
-    });
-});
+        tablaBody.appendChild(trBody);
+        trBody.appendChild(tdFoto);
+        trBody.appendChild(tdNombre);
+        trBody.appendChild(tdMarca);
+        trBody.appendChild(tdModelo);
+        trBody.appendChild(tdKilometraje);
+        trBody.appendChild(tdPrecio);
+
+
+    })
+})
